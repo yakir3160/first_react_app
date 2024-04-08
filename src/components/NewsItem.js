3852aa5a9e7ec4dd6e15e251b643f271d0ca4c26
yakir3160 <132565923+yakir3160@users.noreply.github.com>
2024-04-08 13:27:49 +0300

    update

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const NewsItem = (props) => {
+    const [addresses,setAddresses] = useState(props.company.addresses||[])
+
     return (
-        <div className={"row rowItem box"}>
+        <div className={"row rowItem box text-center"}>
             <div className={"col-lg-4 box"}>
                 <p className='title'>Company name: <p className='text'>{props.company.name}</p></p>
+                <p className='country'>{props.company.country}</p>
             </div>
             <div className={"col-lg-4 box "}>
                 <p className='title'>Email: <p className='text'>{props.company.email}</p></p>
 
             </div>
-            <div className={"col-lg-4  box"}>
-            <p className='title'>Snifim: </p>
+            <div className="col-lg-4 box">
+                <p className="title">Addresses:</p>
+                {addresses.map((address) => (
+                    <p key={address.id} className="text">
+                        {address.streetName + "  "}
+                        {address.zipcode}
+                    </p>
+                ))}
             </div>
         </div>
     );
